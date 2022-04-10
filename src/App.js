4d46859fe9f0ec36d78b6bc8a751b89a78465199
Rosemberg <rosemberc.g.02@gmail.com>
2022-04-10 04:53:28 -0500

    prueba2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './sass/main.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
-import Sign_in from './pages/Sign_in';
-import Sign_up from './pages/Sign_up';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/signIn' element={<Sign_in/>}/>
-        <Route path='/signUp' element={<Sign_up/>}/>
       </Routes>
     </Router>
   );
--- src/pages/Sign_up.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Sign_up = () => {
-    return (
-    <div>Sign_up</div>
-    )
-}
-
-export default Sign_up
